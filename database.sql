d64801296c0f53680bc72129bdd3c4eddc946c42
kusano <kusano@users.noreply.github.com>
2017-06-26 02:34:30 +0900

    データベースの定義を追加

---
+++ database.sql
@@ -0,0 +1,19 @@
+create table user(
+    id text,
+    twitter_id text not null,
+    twitter_name text not null,
+    twitter_icon text not null,
+    score integer not null,
+    valid boolean not null,
+    created_at integer not null,
+    updated_at integer not null,
+    primary key (id)
+);
+
+create table solved(
+    id text references user(id),
+    problem text not null,
+    flag text not null,
+    created_at integer not null,
+    primary key(id, problem, flag)
+);
